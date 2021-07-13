3678a7832d2e6e5c31556c07ce6c24bb997c963d
mojo9492 <mojo9492@outlook.com>
2021-07-13 14:54:38 -0700

    Add file output pokemonAttributes

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,18 @@ E - no exceptions at this time*/
 import fs from "fs";
 import fetch from "node-fetch";
 
-const pokemonFileName = process.argv[2];
+const pokemonFileNameHandler = () => {
+    let result;
+
+    if (process.argv[2]) {
+        result = process.argv[2];
+    } else {
+        console.log('\nNo file given as argument, defaulting to test.txt... \n' +
+        'if you want to source your own file, type:\n node . <your file name here>\n to run the file\n');
+        result = './test.txt';
+    }
+    return result;
+}
 
 const pokeTypeSearch = function (textFile) {
     //read file
@@ -44,10 +55,23 @@ const pokeTypeSearch = function (textFile) {
 
                             const pokeCapName = rest.join(cap);
 
+                            let fileToWrite = '';
+
                             if (pokemon.types[1]) {
-                                console.log(`${pokeCapName}: ${pokemon.types[0].type.name}, ${pokemon.types[1].type.name} `)
+                                fileToWrite += `${pokeCapName}: ${pokemon.types[0].type.name}, ${pokemon.types[1].type.name}\n`;
+                            } else {
+                                fileToWrite += `${pokeCapName}: ${pokemon.types[0].type.name}\n`
                             }
-                            else { console.log(`${pokeCapName}: ${pokemon.types[0].type.name}`) }
+                            console.log(fileToWrite)
+
+                            fs.appendFile('./pokemonAttributes.txt', fileToWrite, (err) => {
+                                if (err) {
+                                    const errorMsg = new Error('Something went wrong')
+                                    console.log(errorMsg);
+                                } else {
+                                    console.log('Successfully written file');
+                                }
+                            })
                         } else {
                             console.log('The pokemon on this line doensn\'t exist');
                         }
@@ -63,5 +87,4 @@ const pokeTypeSearch = function (textFile) {
     }
 }
 
-
-pokeTypeSearch(pokemonFileName)
+pokeTypeSearch(pokemonFileNameHandler())
